75decd7c6f636f7119e5b411e1dd28f8c37c5ce4
Ashish Kumar <48367301+Ashishrock9394@users.noreply.github.com>
2023-05-29 10:31:56 +0530

    Score screen Signed-off-by: Ashish Kumar <48367301+Ashishrock9394@users.noreply.github.com>

--- PongGame/src/Score.java
+++ PongGame/src/Score.java
@@ -9,12 +9,12 @@ public class Score extends Rectangle{
         Score.GAME_WIDTH = GAME_WIDTH;
         Score.GAME_HEIGHT = GAME_HEIGHT;
     }
-    public void draw(Graphics g){
+    public void draw(Graphics g) {
         g.setColor(Color.white);
-        g.setFont(new Font("Consolas",Font.PLAIN,60));
-        g.drawLine(GAME_WIDTH/2,0,GAME_WIDTH/2,GAME_HEIGHT);
-        g.drawString(String.valueOf(player1/10)+String.valueOf(player1%5), GAME_WIDTH/2-85, 50);
-        g.drawString(String.valueOf(player2/10)+String.valueOf(player2%5), GAME_WIDTH/2+20, 50);
+        g.setFont(new Font("Consolas", Font.PLAIN, 30));
+        g.drawLine(GAME_WIDTH / 2, 0, GAME_WIDTH / 2, GAME_HEIGHT);
+        g.drawString("PlayerA: " + String.valueOf(player1), GAME_WIDTH / 4 - 110, 25);
+        g.drawString("PlayerB: " + String.valueOf(player2), GAME_WIDTH / 2 + 140, 25);
     }
 
 }
